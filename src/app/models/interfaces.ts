eb08729e9c7c198e32e3ac1d9e31fb262bc5d10d
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-07-04 17:07:42 -0300

    aula 03

--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -2,17 +2,17 @@ export interface Livro {
   title?: string;
   authors?: string[];
   publisher?: string;
-  publishedDate?: Date;
+  publishedDate?: string;
   description?: string;
   previewLink?: string;
-  thumbnail?: string;
+  thumbnail?: ImageLinks;
 }
 
 export interface VolumeInfo {
   title: string;
   authors: string[];
   publisher: string;
-  publishedDate: Date;
+  publishedDate: string;
   description: string;
   pageCount: number;
   printType: string;
@@ -37,10 +37,10 @@ export interface ImageLinks {
 }
 
 export interface Item {
-  volumeInfo: VolumeInfo;
+  volumeInfo: VolumeInfo
 }
 
 export interface LivrosResultado {
   items: Item[];
-  totalItems: number;
+  totalItems: number
 }
