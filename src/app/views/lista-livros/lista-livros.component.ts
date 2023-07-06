8788a79ecd7379409cfc3ce5473f999c8726e2e6
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-07-06 10:44:22 -0300

    Aula 04

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,52 +1,38 @@
 import { Subscription } from 'rxjs';
 import { Component, OnDestroy } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
-import { Livro } from 'src/app/models/interfaces';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent implements OnDestroy{
-
+export class ListaLivrosComponent implements OnDestroy {
   listaLivros: Livro[];
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+  campoBusca: string = '';
+  subscription: Subscription;
+  livro: Livro;
 
-  constructor(private service: LivroService) { }
+  constructor(private service: LivroService) {}
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
+        this.listaLivros = this.livrosResultadoParaLivros(items);
       },
-      error: erro => console.error(erro),
-    }
-
-    )
+      error: (erro) => console.error(erro),
+    });
   }
 
-  livrosResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = []
-
-    items.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
-      })
-    })
-
-    return livros
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe()
+    this.subscription.unsubscribe();
   }
 }
