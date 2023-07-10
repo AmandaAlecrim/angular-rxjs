c92e2a8d019d4dd95bf6ed85dd4664c91abdaa33
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-07-10 11:19:19 -0300

    Aula 06

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,18 +1,22 @@
 import {
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  map,
   switchMap,
+  map,
   tap,
+  filter,
+  debounceTime,
+  throwError,
+  catchError,
+  EMPTY,
+  of,
 } from 'rxjs';
 import { Component } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
-import { Item } from 'src/app/models/interfaces';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { FormControl } from '@angular/forms';
 
 const PAUSA = 300;
+
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
@@ -20,17 +24,45 @@ const PAUSA = 300;
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) {}
+
+  // totalDeLivros$ = this.campoBusca.valueChanges
+  // .pipe(
+  //   debounceTime(PAUSA),
+  //   filter((valorDigitado) => valorDigitado.length >= 3),
+  //   tap(() => console.log('Fluxo inicial')),
+  //   switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+  //   map(resultado => this.livrosResultado = resultado),
+  //   catchError(erro => {
+  //     console.log(erro)
+  //     return of()
+  //   })
+  // )
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
     filter((valorDigitado) => valorDigitado.length >= 3),
     tap(() => console.log('Fluxo inicial')),
-    distinctUntilChanged(),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    tap(() => console.log('Requisição servidor')),
-    map((items) => this.livrosResultadoParaLivros(items))
+    map((resultado) => (this.livrosResultado = resultado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map((resultado) => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((erro) => {
+      // this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'
+      // return EMPTY
+      console.log(erro);
+      return throwError(
+        () =>
+          new Error(
+            (this.mensagemErro =
+              'Ops, ocorreu um erro. Recarregue a aplicação!')
+          )
+      );
+    })
   );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
